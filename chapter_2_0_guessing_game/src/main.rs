cfbc556ee2d19e04a1d39894593af3f58ef7c6fb
Jan Bataclan <jebataclan03@gmail.com>
2024-07-12 00:02:29 +0800

    Chapter 2 Guessing Game: Finished

--- chapter_2_0_guessing_game/src/main.rs
+++ chapter_2_0_guessing_game/src/main.rs
@@ -1,20 +1,50 @@
-use std::io;
+use std::{cmp::Ordering, io};
 use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let secret_number: u32 = rand::thread_rng().gen_range(1..=100);
 
     // println!("The secret number is {}.", secret_number);
 
-    println!("Please input your guess.");
+    let mut retries: u32 =  10;
 
-    let mut guess = String::new();
+    while retries > 0 {
+        let mut guess: String = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line.");
+        println!("Please input your guess: ");
 
-    println!("You guessed : {}", guess);
+        // Use this if you want your input to be on the
+        // same line as the print your print output.
+        // print!("Please input your guess: ");
+        // let _ = io::stdout().flush();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line.");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(err) => {
+                println!("Failed to parse input. Err: {}", err.to_string());
+                continue;
+            }
+        };
+
+        println!("You guessed : {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+        }
+
+        retries -= 1;
+
+        println!("Retries left: {}", retries);
+    }
 }
